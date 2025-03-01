b90835643b963cffd4cf3a18d67a3f1048a7a846
Lethabo Mokoma <106216950+Morbius24@users.noreply.github.com>
2025-03-01 03:54:27 +0200

    Create Hello_Lethabo.sh

---
+++ Day 1/Hello_Lethabo.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Greetings
+echo "Mariam: Hello Lethabo! How're you ?"
+
+#Reply to Greetings
+echo "Lethabo: Hi Mariam, i'm Great and yourself ?"
+
+# Reply from Prince
+echo "Mariam: Very well thank GOD!"
